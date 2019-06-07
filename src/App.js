cff164d952ffe64c1009de940483f4f6f1ed923e
Alson Shareef <alson.shareef@gmail.com>
2019-06-07 20:47:31 +0800

    Refactored to class component with state, click handler and passing state as props to Person component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import Person from "./People/Person";
 
-function App() {
-	return (
-		<div className="App">
-			<h1>React App</h1>
-		</div>
-	);
+export default class App extends Component {
+	// Main app component state.
+	state = {
+		persons: [{ name: "Alson", age: 23, hobbies: "Web Development" }],
+		buttonName: "Beep"
+	};
+
+	// Click handler example which changes the apps state.
+	clickHandler = () => {
+		if (this.state.buttonName === "Beep") {
+			this.setState({ buttonName: "Bloop" });
+		} else {
+			this.setState({ buttonName: "Beep" });
+		}
+	};
+
+	// Main app render method.
+	render() {
+		return (
+			<div className="App">
+				<h1>React App</h1>
+				<button onClick={this.clickHandler}>Click Me</button>
+				<button>{this.state.buttonName}</button>
+				<p>This is my first React App.</p>
+				<Person
+					name={this.state.persons[0].name}
+					age={this.state.persons[0].age}
+				>
+					Hobbies: {this.state.persons[0].hobbies}
+				</Person>
+			</div>
+		);
+	}
 }
-
-export default App;
