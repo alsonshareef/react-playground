0dd9a490dfb5b238cd106f093d15c8597c23f256
Alson Shareef <alson.shareef@gmail.com>
2019-06-14 15:18:27 +0800

    Person component now destructures the props object and then inserts the variables instead of using dot syntax

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Person.css";
 
-export const Person = props => {
+export const Person = ({ name, age, nameChange, deletePerson, children }) => {
 	return (
 		<div className="person">
-			<p onClick={props.clickName}>My name is {props.name}.</p>
-			<input onChange={props.nameChange} value={props.name} />
-			<p>I am {props.age} years old.</p>
-			<p>{props.children}</p>
+			<p>My name is {name}.</p>
+			<input onChange={nameChange} value={name} />
+			<p>I am {age} years old.</p>
+			<p>{children}</p>
+			<button onClick={deletePerson}>Delete</button>
 		</div>
 	);
 };
