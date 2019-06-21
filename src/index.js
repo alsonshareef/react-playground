ff0f3b0a2f5952e09c108517e7fc6ee7c5c8b8d5
Alson Shareef <alson.shareef@gmail.com>
2019-06-21 17:58:49 +0800

    Passed appTitle prop to App component

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import App from "./containers/App";
 // import Assignment from "./Assignments/Assignment2/Assignment";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+	<App appTitle={"Person Manager"} />,
+	document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
