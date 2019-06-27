41456658a085bf6bac048967d071bdbe22d6f881
Alson Shareef <alson.shareef@gmail.com>
2019-06-27 16:24:51 +0800

    A custom functional HOC to return a function which returns a div holding any component thats passed in. The div will also apply any styles that are passed in through 2nd argument.

--- src/HOC/WithClasses.js
+++ src/HOC/WithClasses.js
@@ -4,8 +4,12 @@
 
 import React from "react";
 
-const WithClasses = props => {
-	return <div className={props.classes}>{props.children}</div>;
+const withClasses = (WrappedComponent, className) => {
+	return props => (
+		<div className={className}>
+			<WrappedComponent {...props} />
+		</div>
+	);
 };
 
-export default WithClasses;
+export default withClasses;
